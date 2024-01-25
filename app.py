8d0775001f9c594363992b7385e44b09917ac2e2
Roukaia Khelifi <rWX1318111@china.huawei.com>
2024-01-25 14:11:31 +0100

    sort and remove duplicates

--- app.py
+++ app.py
@@ -48,9 +48,10 @@ def modify_dataframe(df):
     df = df[df['K3001:Failed SDCCH Seizures due to Busy SDCCH'] >= 10]
     df['Nomber de jour Failure > 10 sur 7'] = df['Cell Name'].apply(lambda x: df[df['Cell Name'] == x].shape[0])
     df['Integrity'] = '100%'
-    df = df[df['Nomber de jour Failure > 10 sur 7']>= 5 ].sort_values(by = 'K3001:Failed SDCCH Seizures due to Busy SDCCH', ascending=False).drop_duplicates('Cell Name')
+    
     return df
-
+def save_worst_cell(df):
+    df = df[df['Nomber de jour Failure > 10 sur 7']>= 5 ].sort_values(by = 'K3001:Failed SDCCH Seizures due to Busy SDCCH', ascending=False).drop_duplicates('Cell Name')
 def main():
     st.title("Excel Sheet Support")
     st.write("""This app is a support of excel sheet modifications and information extraction, you have to upload an excel sheet that has these kind of columns : 'Cell Name',
@@ -69,8 +70,14 @@ def main():
             modified_df = modify_dataframe(df)
             st.dataframe(modified_df)
             buffer = BytesIO()
+            worst_cell = save_worst_cell(modified_df)
+            # save in a excel the first sheet has modified_df and the second has worst_cell with the name sheet1 and sheet2
+            writer = pd.ExcelWriter(buffer, engine='xlsxwriter')
+            modified_df.to_excel(writer, sheet_name='SDCCH Congestion', index=False)
+            worst_cell.to_excel(writer, sheet_name='SDCCH Worst Cells', index=False)
+            writer.save()
 
-            modified_df.to_excel(buffer, index=False, engine='openpyxl')
+            #modified_df.to_excel(buffer, index=False, engine='openpyxl')
             #modified_df.to_excel('modified_excel.xlsx', sheet_name='Sheet1', index=False)
             buffer.seek(0)
             st.download_button(
