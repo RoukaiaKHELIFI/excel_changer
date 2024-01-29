9215e673fbaaccacc90a8ac9953013d5b2a0ad64
Roukaia Khelifi <rWX1318111@china.huawei.com>
2024-01-29 16:17:07 +0100

    More functions

--- app.py
+++ app.py
@@ -36,12 +36,11 @@
 
 
 ###################################
-
-
 import streamlit as st
 import pandas as pd
 from io import BytesIO
 # import openpyxl
+import numpy as np
 
 def modify_dataframe(df):
     df = df.dropna()
@@ -50,9 +49,45 @@ def modify_dataframe(df):
     df['Integrity'] = '100%'
     
     return df
+
 def save_worst_cell(df):
     df = df[df['Nomber de jour Failure > 10 sur 7']>= 5 ].sort_values(by = 'K3001:Failed SDCCH Seizures due to Busy SDCCH', ascending=False).drop_duplicates('Cell Name')
     return df
+
+def Target_cell_sdcch(df):
+    conditions = [
+    (df['Actual Cell SDCCH Channel Maximum'] < 30),
+    (df['Actual Cell SDCCH Channel Maximum'] < 60),
+    (df['Actual Cell SDCCH Channel Maximum'] < 80),
+    (df['Actual Cell SDCCH Channel Maximum'] >= 80)
+]
+    choices = [30, 60, 80, 120]
+    df['Target Cell SDCCH Channel Maximum'] = np.select(conditions, choices, default=0)
+    return df
+
+def script1(df):
+    for i, j in df.iterrows():
+        df.at[i,'Script 1'] = f"SET GCELLCHMGBASIC:IDTYPE=BYID,CELLID={j['Cell CI']},CELLMAXSD={j['Target Cell SDCCH Channel Maximum']};"
+    return df
+
+def script2(df):
+    for i, j in df.iterrows():
+        df.at[i,'Script 2'] = j['Script 1']+"{"+j["GBSC"]+"}"
+    return df
+
+def to_excel(df):
+    output = BytesIO()
+    writer = pd.ExcelWriter(output, engine='openpyxl')
+    df.to_excel(writer, index=False, sheet_name='Sheet1')
+    workbook = writer.book
+    worksheet = writer.sheets['Sheet1']
+    format1 = workbook.add_format({'num_format': '0.00'}) 
+    worksheet.set_column('A:A', None, format1)  
+    writer.save()
+    processed_data = output.getvalue()
+    return processed_data
+
+
 def main():
     st.title("Excel Sheet Support")
     st.write("""This app is a support of excel sheet modifications and information extraction, you have to upload an excel sheet that has these kind of columns : 'Cell Name',
@@ -60,15 +95,20 @@ def main():
      just like the content of this excel sheet'
              """)
     st.image('example.png', width=700)
-    uploaded_file = st.file_uploader("Upload Excel file", type=["xlsx", "xls"])
-
+    st.sidebar.title("Steps and Choices")
+    st.sidebar.write("""1- Upload the excel sheet that you want to modify""")
+    st.sidebar.write("""2- Upload the configuration files""")
+    st.sidebar.write("""3- Download the modified excel sheet""")
+    
+    uploaded_file = st.file_uploader("Upload KPI 2G BTS Query Result file", type=["xlsx", "xls"])
+    
     if uploaded_file is not None:
         try:
             df = pd.read_excel(uploaded_file)
             df = df.to_csv('csvfile.csv', encoding='utf-8', index=False)
             df = pd.read_csv('csvfile.csv')
-
             modified_df = modify_dataframe(df)
+            st.write('Failed Cells')
             st.dataframe(modified_df)
             buffer = BytesIO()
             worst_cell = save_worst_cell(modified_df)
@@ -76,10 +116,10 @@ def main():
             writer = pd.ExcelWriter(buffer, engine='openpyxl')
             modified_df.to_excel(writer, sheet_name='SDCCH Congestion', index=False)
             worst_cell.to_excel(writer, sheet_name='SDCCH Worst Cells', index=False)
-            #writer.save()
+            st.write('Worst Cells')
+            st.dataframe(worst_cell)
             writer.close()
-            #modified_df.to_excel(buffer, index=False, engine='openpyxl')
-            #modified_df.to_excel('modified_excel.xlsx', sheet_name='Sheet1', index=False)
+
             buffer.seek(0)
             st.download_button(
                     label="Download Excel File",
@@ -87,9 +127,49 @@ def main():
                     file_name="modified_excel.xlsx",
                     key="download_button"
                 )
+            if worst_cell is not None:
+                try:
+                    uploaded_file2 = st.file_uploader("Upload Configuration files", type=["xlsx", "xls"], accept_multiple_files=True)
+                    if uploaded_file2 is not None:
+                        uploaded_data_read = [pd.read_excel(file, sheet_name='GCELLCHMGBASIC', skiprows=[0]) for file in uploaded_file2]
+                        raw_data = pd.concat(uploaded_data_read)
+                        config_data = raw_data[['*Cell Name', 'Cell SDCCH Channel Maximum']]
+                        config_data.to_csv('config_data.csv', index=False)
+                    with st.spinner("Please Wait..."):
+                        conf_df = pd.read_csv('config_data.csv')
+                        st.write('Finding Actual Cell SDCCH Channel Maximum')
+                        worst_cell["Actual Cell SDCCH Channel Maximum"] = worst_cell["Cell Name"].apply(lambda x: conf_df[conf_df["*Cell Name"] == x]["Cell SDCCH Channel Maximum"].values[0])
+                        st.write('Getting Most Repeated Actual Cell SDCCH Channel Maximum')
+                        repeated_critical_actual_cell = worst_cell[ worst_cell['Actual Cell SDCCH Channel Maximum'] >=  np.median(worst_cell['Actual Cell SDCCH Channel Maximum'].value_counts().unique())]
+                        st.write('Calculating Target Cell SDCCH Channel Maximum')
+                        target_cells = Target_cell_sdcch(repeated_critical_actual_cell)
+                        st.write('Generating Scripts')
+                        script_1 = script1(target_cells)
+                        script_2 = script2(script_1)
+                        script_2.to_excel('script.xlsx', index=False)
+                except Exception as e:
+                    st.write(f"Now you can provide the Configuration Data files that has the Cell SDCCH Channel Maximum")
+            sc = pd.read_excel('script.xlsx')
+            buffer2 = BytesIO()
+            writer2 = pd.ExcelWriter(buffer2, engine='openpyxl')
 
+            modified_df.to_excel(writer2, sheet_name='SDCCH Congestion', index=False)
+            worst_cell.to_excel(writer2, sheet_name='SDCCH Worst Cells', index=False)
+            sc.to_excel(writer2, sheet_name='Script', index=False)
+            writer2.close()
+            buffer2.seek(0)
+            st.download_button(
+                        label="Download Script File",
+                        data=buffer2,
+                        file_name="script.xlsx",
+                        key="download_button2"
+                    ) 
         except Exception as e:
             st.error(f"Error: {e}")
+
+    
+                
+          
     st.link_button('Email Me!','https://mail.google.com/mail/u/0/#inbox?compose=DmwnWtMmVfqrkGHslNKWWgMvKPPDhXmSGxWNkCkCWsztBWXJNzvTNcsJJDpLncMXPkrHWGMnzRtV')
     
 if __name__ == "__main__":
